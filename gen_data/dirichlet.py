46207d1ec99410ff58e1f4ff2c853d2c73897488
duclong1009 <duclong10092000@gmail.com>
2023-06-27 22:17:20 +0900

    udt

---
+++ gen_data/dirichlet.py
@@ -0,0 +1,107 @@
+import numpy as np
+import random
+import time
+
+
+
+def iid_divide(l, g):
+    """
+    https://github.com/TalwalkarLab/leaf/blob/master/data/utils/sample.py
+    divide list l among g groups
+    each group has either int(len(l)/g) or int(len(l)/g)+1 elements
+    returns a list of groups
+    """
+    num_elems = len(l)
+    group_size = int(len(l) / g)
+    num_big_groups = num_elems - g * group_size
+    num_small_groups = g - num_big_groups
+    glist = []
+    for i in range(num_small_groups):
+        glist.append(l[group_size * i: group_size * (i + 1)])
+    bi = group_size * num_small_groups
+    group_size += 1
+    for i in range(num_big_groups):
+        glist.append(l[bi + group_size * i:bi + group_size * (i + 1)])
+    return glist
+
+
+def split_list_by_indices(l, indices):
+    """
+    divide list l given indices into len(indices) sub-lists
+    sub-list i starts from indices[i] and stops at indices[i+1]
+    returns a list of sub-lists
+    """
+    res = []
+    current_index = 0
+    for index in indices:
+        res.append(l[current_index: index])
+        current_index = index
+
+    return res
+
+
+def by_labels_non_iid_split(dataset, n_classes, n_clients, n_clusters, alpha, frac=1, seed=1234):
+    """
+    split classification dataset among n_clients. The dataset is split as follow:
+        1) classes are grouped into n_clusters
+        2) for each cluster c, samples are partitioned across clients using dirichlet distribution
+
+    Inspired by the split in "Federated Learning with Matched Averaging"__(https://arxiv.org/abs/2002.06440)
+
+    :param dataset:
+    :type dataset: torch.utils.Dataset
+    :param n_classes: number of classes present in dataset
+    :param n_clients: number of clients
+    :param n_clusters: number of clusters to consider; if it is -1, then n_clusters = n_classes
+    :param alpha: parameter controlling the diversity among clients
+    :param frac: fraction of dataset to use
+    :param seed:
+    :return: list (size n_clients) of subgroups, each subgroup is a list of indices.
+    """
+    if n_clusters == -1:
+        n_clusters = n_classes
+
+    rng_seed = (seed if (seed is not None and seed >= 0) else int(time.time()))
+    rng = random.Random(rng_seed)
+    np.random.seed(rng_seed)
+
+    all_labels = list(range(n_classes))
+    rng.shuffle(all_labels)
+    clusters_labels = iid_divide(all_labels, n_clusters)
+
+    label2cluster = dict()  # maps label to its cluster
+    for group_idx, labels in enumerate(clusters_labels):
+        for label in labels:
+            label2cluster[label] = group_idx
+    # get subset
+    n_samples = int(len(dataset) * frac)
+    selected_indices = np.random.randint(0, len(dataset), n_samples)
+
+    clusters_sizes = np.zeros(n_clusters, dtype=int)
+    clusters = {k: [] for k in range(n_clusters)}
+    for idx in selected_indices:
+        label = dataset[idx]
+        group_id = label2cluster[label]
+        clusters_sizes[group_id] += 1
+        clusters[group_id].append(idx)
+
+    for _, cluster in clusters.items():
+        rng.shuffle(cluster)
+
+    clients_indices = [[] for _ in range(n_clients)]    
+    clients_counts = np.zeros((n_clusters, n_clients), dtype=np.int64)  # number of samples by client from each cluster
+    
+    for cluster_id in range(n_clusters):
+            weights = np.random.dirichlet(alpha=alpha * np.ones(n_clients))
+            clients_counts[cluster_id] = np.random.multinomial(clusters_sizes[cluster_id], weights)
+
+    
+    clients_counts = np.cumsum(clients_counts, axis=1)
+
+    for cluster_id in range(n_clusters):
+        cluster_split = split_list_by_indices(clusters[cluster_id], clients_counts[cluster_id])
+
+        for client_id, indices in enumerate(cluster_split):
+            clients_indices[client_id] += indices
+
+    return clients_indices

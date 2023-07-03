fcc94b3f8e166ba7773cbfb801099d29cb03ddf2
duclong1009 <duclong10092000@gmail.com>
2023-07-03 15:07:58 +0900

    add scaffold

--- test.sh
+++ test.sh
@@ -1,4 +1,1 @@
-
-DATA_DIR="pill_dataset/medium_pilldataset"
-LOG_DIR="Test"
-python main.py --num_rounds 1000 --session_name "cluster_20_alpha_0_1_" --group_name "NII_medium_pilldataset_100client_dirichlet" --proportion 0.3 --algorithm fedfa_algo1 --ratio 0.8 --aggregate "weighted_com" --task medium_pilldataset --num_classes 150 --data_path ${DATA_DIR} --save_folder_path ${LOG_DIR} --idx_path pill_dataset/medium_pilldataset/100client/dirichlet/data_idx_alpha_0.1_cluster_20.json --score all_gnorm_threshold --log_wandb
+python main.py --num_rounds 1000 --session_name "cluster_20_alpha_0_1_" --group_name "NII_medium_pilldataset_100client_dirichlet" --proportion 0.3 --algorithm scaffold_fedalgo1 --ratio 1 --aggregate "weighted_com" --task medium_pilldataset --num_classes 150 --data_path "pill_dataset/medium_pilldataset" --save_folder_path "test" --idx_path pill_dataset/medium_pilldataset/100client/dirichlet/data_idx_alpha_0.1_cluster_20.json --score all_gnorm_threshold --log_wandb

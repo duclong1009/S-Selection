7ea66e09b6c9acbc14d648c74fe0acf24d8f6d58
tien-th <tien.nh205033@sis.hust.edu.vn>
2023-11-27 07:50:59 +0700

    cifar100

--- benchmark/cifar100noise_classification/model/resnet18.py
+++ benchmark/cifar100noise_classification/model/resnet18.py
@@ -76,7 +76,7 @@ class BottleNeck(nn.Module):
     def forward(self, x):
         return nn.ReLU(inplace=True)(self.residual_function(x) + self.shortcut(x))
 
-
+import torchvision
 class Model(FModule):
     def __init__(self):
         super().__init__()
